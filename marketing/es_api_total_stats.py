4c68e19562c743613f151308401c6385e07b30ec
leonidparubets <l>
2022-06-07 15:18:44 +0300

    new format

--- marketing/es_api_total_stats.py
+++ marketing/es_api_total_stats.py
@@ -7,16 +7,18 @@ from google.oauth2 import service_account
 from google.cloud import bigquery
 import os
 import gspread as gs
-import numpy 
+import numpy
 
-
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/Users/leonidparubets/PycharmProjects/Careerist/fabled-sorter-289010-9aef64e16ce3.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = \
+    "/Users/leonidparubets/PycharmProjects/Careerist/fabled-sorter-289010-9aef64e16ce3.json"
 gc = gs.service_account(filename="/fabled-sorter-289010-9aef64e16ce3.json")
-credentials = service_account.Credentials.from_service_account_file("/fabled-sorter-289010-9aef64e16ce3.json")
+credentials = service_account.Credentials.from_service_account_file \
+    ("/Users/leonidparubets/PycharmProjects/Careerist/fabled-sorter-289010-9aef64e16ce3.json")
 
 today = date.today().strftime("%Y-%m-%d")
 
-soup = BeautifulSoup(requests.get("https://api7.esv2.com/v2/Api/SummaryStatistics?apiKey=p5qnbI5yxR2n7aGOf5yB &startDate=2021-01-01&endDate="+today+"&grouping=Date").content, "html.parser")
+soup = BeautifulSoup(requests.get("https://api7.esv2.com/v2/Api/SummaryStatistics?apiKey=p5qnbI5yxR2n7aGOf5yB"
+                                  " &startDate=2021-01-01&endDate=" + today + "&grouping=Date").content, "html.parser")
 
 def stats(list_name):
     list_name = [i.text for i in soup.find_all(list_name)]
@@ -36,12 +38,13 @@ unsubscribes = stats('unsubscribes')
 goals = stats('goals')
 goalsvalue = stats('goalsvalue')
 
-df = pd.DataFrame(list(zip(date, sent[1:], bounced[1:], delivered[1:], opens[1:], uniqueopens[1:], clicks[1:],                     uniqueclicks[1:], clickers[1:], complaints[1:], unsubscribes[1:], goals[1:], goalsvalue[1:])),
-               columns =['date', 'sent', 'bounced','delivered','opens','uniqueopens','clicks', \
-                        'uniqueclicks', 'clickers', 'complaints', 'unsubscribes', 'goals', 'goalsvalue'])
+df = pd.DataFrame(list(zip(date, sent[1:], bounced[1:], delivered[1:], opens[1:], uniqueopens[1:], clicks[1:],
+                           uniqueclicks[1:], clickers[1:], complaints[1:], unsubscribes[1:],
+                           goals[1:], goalsvalue[1:])),
+                  columns=['date', 'sent', 'bounced', 'delivered', 'opens', 'uniqueopens', 'clicks',
+                           'uniqueclicks', 'clickers', 'complaints', 'unsubscribes', 'goals', 'goalsvalue'])
 
-df = df.assign(date = [*map(lambda x : pd.to_datetime(x, format = '%Y-%m-%d'), df['date'])] )
-
+df = df.assign(date=[*map(lambda x: pd.to_datetime(x, format='%Y-%m-%d'), df['date'])])
 
 client = bigquery.Client(project='fabled-sorter-289010')
 
@@ -51,12 +54,7 @@ job_config = bigquery.LoadJobConfig(
 
 # Define table name, in format dataset.table_name
 table = 'Renta_dataset.expertsender_stats'
-#job_config.write_disposition = 'WRITE_APPEND' 
+# job_config.write_disposition = 'WRITE_APPEND'
 
 # Load data to BQ
-job = client.load_table_from_dataframe(df, table, job_config = job_config)
-
-
-
-
-
+job = client.load_table_from_dataframe(df, table, job_config=job_config)
